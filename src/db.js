4542912bf87f483d03b442598c396d14d50bb54b
Ria Nicole Carmin <akimalunar@gmail.com>
2018-03-28 16:38:21 -0700

    Temp fix

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 import { DATABASE_URL } from './config';
 
 export default async () => {
-  const db = await MongoClient.connect(DATABASE_URL);
+  const db = await MongoClient.connect('mongodb://blueOperator:BombasticJewel!@ds151973.mlab.com:51973/pretty-prism-dev');
   return {
     Polishes: db.collection('polishes'),
     Users: db.collection('users'),
